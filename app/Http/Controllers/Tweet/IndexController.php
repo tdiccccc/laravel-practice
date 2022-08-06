1cf683b2325d4b09e2fe4bfdcf6a6d1883c41d74
tdiccccc <99638807+tdiccccc@users.noreply.github.com>
2022-08-06 17:02:29 +0900

    [Add]サービスコンテナ

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
+use App\Services\TweetService; //TweetServiceのインポート
 use App\Models\Tweet;
 use Illuminate\Http\Request;
 
@@ -14,12 +15,13 @@ class IndexController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, TweetService $tweetService)
     {
         //
         //return view('tweet.index',['name' => 'laravel']);
         //view laravelのヘルパー関数
-        $tweets = Tweet::orderBy('created_at', 'DESC')->get();
+        
+        $tweets = $tweetService->getTweets();//つぶやきの一覧を取得 //Tweet::orderBy('created_at', 'DESC')->get();
       //$tweets = Tweet::all()->sortByDesc('created_at'); 上記同様、順番の変更 前者の記述のほうが高速  
         //dd($tweets);
         return view('tweet.index')
