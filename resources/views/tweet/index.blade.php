79818400a7f47b53a8ba232f398d2dd883e4df92
tdiccccc <99638807+tdiccccc@users.noreply.github.com>
2022-08-06 16:25:23 +0900

    [Add]delete

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -12,6 +12,10 @@
   <h1>つぶやきアプリ</h1>
   <div>
     <p>投稿フォーム</p>
+    @if (session('feedback.success'))
+        <p style="color: green">{{ session('feedback.success') }}</p>
+    @endif
+
     <form action="{{ route('tweet.create') }}" method="post">
       @csrf
       <label for="tweet-content">つぶやき</label>
@@ -27,6 +31,11 @@
         <summary>{{ $tweet->content }}</summary>
         <div>
             <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id])}}">編集</a>
+            <form action="{{ route('tweet.delete', ['tweetId' => $tweet->id]) }}" method="post">
+              @method('DELETE')
+              @csrf
+              <button type="submit">削除</button>
+            </form>
         </div>
       </details>
       <!-- <p>{{ $tweet->content }}</p> -->
